c545338593b4bc5eb076ea2b04e7b6a922baaae3
krishnaja-b <b.l.krishnaja@gmail.com>
2023-04-15 08:44:33 +0530

    giving input to a function

--- functions.sh
+++ functions.sh
@@ -1,6 +1,12 @@
 # declare a function
 xyz(){
   echo "hello from function"
+  echo first argument - $1
+  echo second argument - $2
+  echo number of arguments - $#
+  echo all arguments - $*
 }
+
+## main program
 ## call a function
-xyz
+xyz 123 456
