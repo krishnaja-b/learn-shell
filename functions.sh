d0965518a01548f2109dcaa9b72cb3b0e263989a
krishnaja-b <b.l.krishnaja@gmail.com>
2023-04-15 09:15:37 +0530

    vars of main pro can b overwriten and viceversa

--- functions.sh
+++ functions.sh
@@ -5,12 +5,13 @@ xyz(){
   echo second argument - $2
   echo number of arguments - $#
   echo all arguments - $*
-  echo value of a = $a
+
+
   a=300
+  echo value of a= $a
   b=200
 
 }
-
 ## main program
 ## call a function
 a=120
