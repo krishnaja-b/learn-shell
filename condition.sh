3534a9817a94aa8d0821a0a25945c9c67860fc9b
krishnaja-b <b.l.krishnaja@gmail.com>
2023-04-21 09:05:58 +0530

    using quotes in expression

--- condition.sh
+++ condition.sh
@@ -1,10 +1,10 @@
 number=$1
 string=$2
 
-if [ ${number} -eq 5 ]; then
+if [ "${number}" -eq 5 ]; then
   echo number is 5
   fi
 
-if [ ${$string1 } == abc  ]; then
+if [ "${$string1 }" == abc  ]; then
   echo string is abc
     fi
